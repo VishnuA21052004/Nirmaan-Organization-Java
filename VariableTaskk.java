61af70fda64602ab8237cb6cbc2b4529b3b662cc
VishnuA21052004 <peteranbu05@gmail.com>
2024-11-21 12:47:44 +0530

    day2

---
+++ VariableTaskk.java
@@ -0,0 +1,31 @@
+public class VariableTaskk
+{
+public static void main(String[]args){
+String name1="Vishnu";
+String adjective1="happy";
+String adjective2="wonderful";
+String noun1="birds";
+String noun2="chocolate";
+String name2="jump";
+String place1="guitar";
+String adjective3="ram";
+String noun5="bboy";
+String noun6="goal";
+String noun4="hawali";
+String noun3="ice";
+String verb1="robots";
+
+int number=5;
+
+
+
+
+
+
+
+
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2+ " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to" + verb1 + " tothe rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ".Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " +name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+ System.out.print(story);
+}
+}
